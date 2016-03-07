3c056d4ef8d3f4155158f653d1e2a89eb7ac0b73
Caleb Cauthon <calebcauthon@gmail.com>
2016-03-06 19:20:51 -0600

    Add contact form functionality

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'http://rubygems.org'
 gem 'sinatra'
+gem 'mongoid', '~> 5.1.0'
--- app.rb
+++ app.rb
@@ -1,7 +1,31 @@
 require 'sinatra'
+require 'mongoid'
+
+set :environment, :development
+Mongoid.load!('mongoid.yml')
+
+class ContactForm
+  include Mongoid::Document
+
+  store_in collection: "contact_forms"
+
+  field :name, type: String
+  field :email, type: String
+  field :phone, type: String
+  field :message, type: String
+end
 
 set :public_folder, 'public'
 
 get '/' do
   erb :index
+end
+
+post '/contact' do
+  ng_params = JSON.parse(request.body.read)
+  submission = ContactForm.new(:email => ng_params["email"],
+      :name => ng_params['name'],
+      :phone => ng_params['phone'],
+      :message => ng_params['message'])
+  submission.save
 end
