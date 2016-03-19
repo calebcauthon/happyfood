f4956559063c38fab49c60ed99549136881c8033
Caleb Cauthon <calebcauthon@gmail.com>
2016-03-19 00:02:15 -0500

    Add stripe payments

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'mongoid'
 require 's3'
+require 'stripe'
 
 set :environment, :development
 Mongoid.load!('mongoid.yml')
@@ -33,7 +34,7 @@ end
 set :public_folder, 'public'
 
 get '/' do
-  erb :index
+  erb :index, locals: { publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'] }
 end
 
 post '/contact' do
@@ -117,4 +118,64 @@ post '/admin/add-recipe-image' do
   new_object.save
 
   { url: new_object.url }.to_json
+end
+
+def get_verified_cost(cart)
+  total_cost = 0
+  cart.each do |item|
+    recipe = Recipe.find(item["recipe"]["_id"]["$oid"])
+    serving_option = recipe.serving_options.first do |option|
+      item["serving_option"]["servings"] == option.servings
+    end
+
+    total_cost = total_cost + serving_option["dollars"].to_i
+  end
+
+  total_cost
+end
+
+def stripe_charge(cost, token, cart)
+  Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+  stripe_params = {
+    :amount => cost * 100,
+    :currency => "usd",
+    :source => token,
+    :description => "Happyfood Web Charge",
+    :metadata => {
+      cart: cart.map do |item|
+        "#{item["recipe"]["name"]} (#{item["serving_option"]["servings"]} servings)"
+      end.join(" | ")
+    }
+  }
+
+  begin
+    return Stripe::Charge.create(stripe_params)
+  rescue Stripe::CardError => e
+    return e
+  rescue Exception => e
+    return e
+  end
+end
+
+post '/charge' do
+  ng_params = JSON.parse(request.body.read)
+
+  verified_cost = get_verified_cost(ng_params["cart"])
+
+  if(verified_cost != ng_params["browser_total_cost"].to_i)
+    status 401
+    return {
+      expected: verified_cost,
+      got: ng_params["browser_total_cost"].to_i
+    }.to_json
+  end
+
+  result = stripe_charge(verified_cost, ng_params["stripeToken"], ng_params["cart"])
+
+  if(result)
+    status 200
+    return result.to_json
+  else
+    status 402
+  end
 end
